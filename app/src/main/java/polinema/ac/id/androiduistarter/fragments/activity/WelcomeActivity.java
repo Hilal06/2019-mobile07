5f1506dd9fbd221b1941e5cb8008cebbba957f59
Hilal06 <rifaulhilal06@gmail.com>
2019-10-17 01:08:01 +0700

    add navigation

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/activity/WelcomeActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activity/WelcomeActivity.java
@@ -1,16 +1,166 @@
 package polinema.ac.id.androiduistarter.fragments.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import polinema.ac.id.androiduistarter.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private LinearLayout dotslayout;
+    private TextView[] dots;
+    private int[] layout;
+    private Button btnSkip, btnNext;
+    private MyViewPagerAdapter myViewPagerAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        viewPager = findViewById(R.id.view_pager);
+        dotslayout = findViewById(R.id.layoutDots);
+        btnNext = findViewById(R.id.btn_next);
+        btnSkip = findViewById(R.id.btn_skip);
+
+        layout = new int[] {
+                R.layout.welcome_slide1,
+                R.layout.welcome_slide2,
+                R.layout.welcome_slide3,
+                R.layout.welcome_slide4
+        };
+        addBottomDots(0);
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewOnPageChangeListener);
+
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchHomeScreen();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking for last page
+                // if last page home screen will be launched
+                int current = getItem(+1);
+                if (current < layout.length) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);
+                } else {
+                    launchHomeScreen();
+                }
+            }
+        });
+
+
     }
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layout.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        dotslayout.removeAllViews();
+        for (int i = 0; i<dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorInactive[currentPage]);
+            dotslayout.addView(dots[i]);
+        }
+        if (dots.length > 0) {
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+        }
+    }
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+    private void launchHomeScreen() {
+        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+        finish();
+    }
+
+    ViewPager.OnPageChangeListener viewOnPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layout.length - 1) {
+                // last page. make button text to GOT IT
+                btnNext.setText(getString(R.string.start));
+                btnSkip.setVisibility(View.GONE);
+            } else {
+                // still pages are left
+                btnNext.setText(getString(R.string.next));
+                btnSkip.setVisibility(View.VISIBLE);
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layout[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layout.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+
 }
+
+
